9857a76eb47c5894a82153039e883af4d10b2b4c
alexmpark <alexandros.mparkolias@gmail.com>
2020-11-11 20:36:16 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="/ADISE20_123970/assets/css/styles.css">
+    </head>
+
+
+    <body>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="tavli">
+                        <div id="m1" class="pouli mavro"></div>
+                        <div id="m2" class="pouli mavro"></div>
+                        <div id="m3" class="pouli mavro"></div>
+                        <div id="m4" class="pouli mavro"></div>
+                        <div id="m5" class="pouli mavro"></div>
+                        <div id="m6" class="pouli mavro"></div>
+                        <div id="m7" class="pouli mavro"></div>
+                        <div id="m8" class="pouli mavro"></div>
+                        <div id="m9" class="pouli mavro"></div>
+                        <div id="m10" class="pouli mavro"></div>
+                        <div id="m11" class="pouli mavro"></div>
+                        <div id="m12" class="pouli mavro"></div>
+                        <div id="m13" class="pouli mavro"></div>
+                        <div id="m14" class="pouli mavro"></div>
+                        <div id="m15" class="pouli mavro"></div>
+                        
+                        <div id="a1" class="pouli aspro"></div>
+                        <div id="a2" class="pouli aspro"></div>
+                        <div id="a3" class="pouli aspro"></div>
+                        <div id="a4" class="pouli aspro"></div>
+                        <div id="a5" class="pouli aspro"></div>
+                        <div id="a6" class="pouli aspro"></div>
+                        <div id="a7" class="pouli aspro"></div>
+                        <div id="a8" class="pouli aspro"></div>
+                        <div id="a9" class="pouli aspro"></div>
+                        <div id="a10" class="pouli aspro"></div>
+                        <div id="a11" class="pouli aspro"></div>
+                        <div id="a12" class="pouli aspro"></div>
+                        <div id="a13" class="pouli aspro"></div>
+                        <div id="a14" class="pouli aspro"></div>
+                        <div id="a15" class="pouli aspro"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    </body>
+</html>
